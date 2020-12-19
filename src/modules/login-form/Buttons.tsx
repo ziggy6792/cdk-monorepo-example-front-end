e9ade22946e7316fe16958f994f56385c3fd6664
Simon <simonaverhoeven@gmail.com>
2020-12-19 15:48:12 +0800

    changed profile to FC

--- src/modules/login-form/Buttons.tsx
+++ src/modules/login-form/Buttons.tsx
@@ -11,7 +11,7 @@ interface IButtonsProps {
   updateFormState: (formState: string) => void;
 }
 
-const Buttons: React.FC<IButtonsProps> = (props) => {
+const Buttons: React.FC<IButtonsProps> = ({ updateFormState }) => {
   return (
     <Grid container direction='column' justify='center' alignItems='center' style={{ height: '100%' }}>
       <Grid item>
@@ -20,94 +20,12 @@ const Buttons: React.FC<IButtonsProps> = (props) => {
         </Button>
       </Grid>
       <Grid item>
-        <Button color='primary' endIcon={<Email />} onClick={() => props.updateFormState('email')}>
+        <Button color='primary' endIcon={<Email />} onClick={() => updateFormState('email')}>
           Sign in with Email
         </Button>
       </Grid>
     </Grid>
-
-    // <div>
-    //   <div style={styles.container as any}>
-    //     <button
-    //       style={{ ...styles.button, ...styles.facebook } as any}
-    //       onClick={() => {
-    //         // props.auth.onLogin({ authType: 'Facebook' });
-    //         console.log({ authType: 'Facebook' });
-    //       }}
-    //     >
-    //       <FaFacebook color="white" />
-    //       <p style={styles.text as any}>Sign in with Facebook</p>
-    //     </button>
-    //     <button style={{ ...styles.button, ...styles.email }} onClick={() => props.updateFormState('email')}>
-    //       <FaEnvelope color="white" />
-    //       <p style={{ ...styles.text } as any}>Sign in with Email</p>
-    //     </button>
-    //   </div>
-    // </div>
   );
-};
-
-const styles = {
-  container: {
-    height: '80vh',
-    width: '100vw',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
-  button: {
-    width: '100%',
-    maxWidth: 250,
-    marginBottom: 10,
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    padding: '0px 16px',
-    borderRadius: 2,
-    boxShadow: '0px 1px 3px rgba(0, 0, 0, .3)',
-    cursor: 'pointer',
-    outline: 'none',
-    border: 'none',
-    minHeight: 40,
-  },
-  facebook: {
-    backgroundColor: '#3b5998',
-  },
-  email: {
-    backgroundColor: '#db4437',
-  },
-  checkAuth: {
-    backgroundColor: '#02bd7e',
-  },
-  hostedUI: {
-    backgroundColor: 'rgba(0, 0, 0, .6)',
-  },
-  signOut: {
-    backgroundColor: 'black',
-  },
-  withAuthenticator: {
-    backgroundColor: '#FF9900',
-  },
-  icon: {
-    height: 16,
-    marginLeft: -1,
-  },
-  text: {
-    color: 'white',
-    fontSize: 14,
-    marginLeft: 10,
-    fontWeight: 'bold',
-  },
-  blackText: {
-    color: 'black',
-  },
-  grayText: {
-    color: 'rgba(0, 0, 0, .75)',
-  },
-  orangeText: {
-    color: '#FF9900',
-  },
 };
 
 export default Buttons;
--- src/screens/profile-screen/profile-screen.tsx
+++ src/screens/profile-screen/profile-screen.tsx
@@ -2,89 +2,40 @@
 import React, { useEffect, useState, Fragment, Component } from 'react';
 
 import { Hub } from 'aws-amplify';
-import { FaSignOutAlt } from 'react-icons/fa';
 
-import { withApollo } from 'react-apollo';
-
+import { Button } from '@material-ui/core';
 import Spinner from '../../components/spinner';
 import Buttons from '../../modules/login-form/Buttons';
-import withAuthenticator from '../../hoc/withAuthenticator';
 import LoginForm from '../../modules/login-form';
-
-interface Props {
-  auth: any;
-  client: any;
-}
 
 interface State {
   formState: string;
 }
-class ProfileScreen extends React.Component<Props, State> {
-  myListener: (data: any) => void;
-
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      formState: 'base',
-    };
-    this.myListener = (data) => {
-      const { payload } = data;
-      // if (payload.event === 'signIn') {
-      //   setImmediate(() => {
-      //     this.setState({ formState: null });
-      //   });
-      // }
-      // if (payload.event !== 'signUp') {
-      //   this.setState({ formState: null });
-      //   window.location.reload(false);
-      // }
-    };
+const ProfileScreen: React.FC = () => {
+  const [formState, setFormSate] = useState('base');
+  // This renders the custom form
+  if (formState === 'email') {
+    return <LoginForm />;
+  }
+  if (!formState) {
+    return <Spinner />;
   }
 
-  componentDidMount() {
-    Hub.listen('auth', this.myListener);
-  }
+  const isAuthenticated = false;
 
-  componentWillUnmount() {
-    Hub.remove('auth', this.myListener);
-  }
+  return (
+    <>
+      {isAuthenticated && (
+        <>
+          <h4>Welcome</h4>
+          <Button>sign out</Button>
+        </>
+      )}
 
-  render() {
-    // This renders the custom form
-    if (this.state.formState === 'email') {
-      return <LoginForm />;
-    }
-    if (!this.state.formState) {
-      return <Spinner />;
-    }
+      {!isAuthenticated && <Buttons updateFormState={setFormSate} />}
+      {isAuthenticated && <div>Is Authenticated</div>}
+    </>
+  );
+};
 
-    const isAuthenticated = false;
-
-    return (
-      <>
-        {isAuthenticated && (
-          <>
-            <h4>Welcome {this.props.auth.user.nameDisplayText}</h4>
-            <button
-              onClick={async () => {
-                // this.setState({ formState: null });
-
-                // this.setState({ formState: null });
-                this.props.client.cache.reset();
-                await this.props.auth.onLogout();
-              }}
-            >
-              <FaSignOutAlt color='white' />
-              <p>Sign Out</p>
-            </button>
-          </>
-        )}
-
-        {!isAuthenticated && <Buttons updateFormState={(newState: any) => this.setState((prevState) => ({ ...prevState, formState: newState }))} />}
-        {isAuthenticated && <div>Is Authenticated</div>}
-      </>
-    );
-  }
-}
-
-export default withApollo(ProfileScreen);
+export default ProfileScreen;
