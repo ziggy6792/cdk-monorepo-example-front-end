afde3ebeaa50bb7941fc38c99ce93dc5e08b53f9
Simon <simonaverhoeven@gmail.com>
2020-12-29 02:32:42 +0800

    cleanup

--- src/util/aws-config.ts
+++ src/util/aws-config.ts
@@ -1,13 +1,14 @@
 import _ from 'lodash';
-import awsconfig from '../cdk-exports';
+import cdkExports from '../cdk-exports';
 
-const modifiedAwsConfig = _.cloneDeep(awsconfig);
+const awsConfig = _.cloneDeep(cdkExports);
 
-modifiedAwsConfig.aws_graphqlEndpoint_authNone = 'http://localhost:3100/lambda-gq-resolver/graphql';
-modifiedAwsConfig.aws_graphqlEndpoint_authRole = 'http://localhost:3100/lambda-gq-resolver/graphql';
-modifiedAwsConfig.aws_graphqlEndpoint_authUser = 'http://localhost:3100/lambda-gq-resolver/graphql';
+// Overwrite to local endpoint
+awsConfig.aws_graphqlEndpoint_authNone = 'http://localhost:3100/lambda-gq-resolver/graphql';
+awsConfig.aws_graphqlEndpoint_authRole = 'http://localhost:3100/lambda-gq-resolver/graphql';
+awsConfig.aws_graphqlEndpoint_authUser = 'http://localhost:3100/lambda-gq-resolver/graphql';
 
-modifiedAwsConfig.oauth.redirectSignIn = `${window.location.protocol}//${window.location.host}${awsconfig.oauth.redirectSignIn}`;
-modifiedAwsConfig.oauth.redirectSignOut = `${window.location.protocol}//${window.location.host}${awsconfig.oauth.redirectSignIn}`;
+awsConfig.oauth.redirectSignIn = `${window.location.protocol}//${window.location.host}${cdkExports.oauth.redirectSignIn}`;
+awsConfig.oauth.redirectSignOut = `${window.location.protocol}//${window.location.host}${cdkExports.oauth.redirectSignIn}`;
 
-export default modifiedAwsConfig;
+export default awsConfig;
